2906d28392f931099465302871b12f717fe42cc6
97noelia <noelia.fd.97@gmail.com>
2020-02-26 20:08:53 +0100

    hecho hasta mostrar el mapa

--- frontend/src/app/componentes/mapa/mapa.component.ts
+++ frontend/src/app/componentes/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-mapa',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapa.component.css']
 })
 export class MapaComponent implements OnInit {
-
+  private map: any;
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3
+    });
+  }
+
 
 }
